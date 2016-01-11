cd5fd06a961c1c9aa105be2568d464c20f4d646d
Hilario Fernandes <hilario.fernandes@cortex-intelligence.com>
2016-01-11 12:08:46 +0000

    Updated greasemonkey script

--- MyEpisodesDownloader.user.js
+++ MyEpisodesDownloader.user.js
@@ -1,91 +1,89 @@
 // ==UserScript==
 // @name        MyEpisodes Download Link
+// @description Adds download links to MyEpisodes website
+// @namespace   https://greasyfork.org/en/users/26838-hilario-fernandes
 // @include     http://www.myepisodes.com/epslist*
-// @version     0.1
+// @version     1.0
 // @Author      hfernandes
+// @license     http://creativecommons.org/licenses/by-nc-sa/3.0/us/
 // @grant       none
 // ==/UserScript==
+'use strict';
 
+// --------------- Tracker --------------- 
+// FORMAT: Tracker(shortName, iconURL, searchURL, useNumbers)
+//     shortName - Alt display name for link
+//     iconURL - icon displayed for link created
+//     searchURL - URL that search string is appended to
+//     useNumbers - if true adds the episode number to the search URL
+//var tracker = new Tracker("EZTV", "http://eztv.ag/favicon.ico", "https://eztv.ag/search/", false);
+var tracker = new Tracker("Kickass", "http://kastatic.com/images/favicon.ico", "https://kickass.to/usearch/?field=seeders&sorder=desc&q=", true);
 
-(function() {
-  function Tracker(shortname, icon, searchurl, useNumbers) {
-    this.shortname = shortname;
-    this.icon = icon;
-    this.searchurl = searchurl;
-    this.useNumbers = useNumbers;
-    
-    this.getHTML = function (query, episode) {
-      var tShortname = this.shortname;
-      var tIcon = this.icon;
-      var tSearchURL = this.searchurl;
-      var tUseNumbers = this.useNumbers;
-                  
-      // Add episode numbers if enabled
-      if(tUseNumbers){
-        search = query + " " + episode;
-      } else {
-        search = query;
-      }
-      
-      var html = "<a target=\"_blank\" href=\"" + tSearchURL;  
-      html += escape(search);
-      html += "\">";
+function Tracker(shortname, icon, searchurl, useNumbers) {
+	this.shortname = shortname;
+	this.icon = icon;
+	this.searchurl = searchurl;
+	this.useNumbers = useNumbers;
 
-      if (tIcon != "") {
-        html += "<img width=\"14\" heigth=\"14\" border=\"0\" src=\"" + tIcon + "\" alt=\"" + tShortname + "\">";
-      } else {
-        html += tShortname;
-      }
-      html += "</a>";
-      return html;
-    }
-    
-  }
+	this.getHTML = function (query, episode) {
+		var tShortname = this.shortname;
+		var tIcon = this.icon;
+		var tSearchURL = this.searchurl;
+		var tUseNumbers = this.useNumbers;
+		          
+		// Add episode numbers if enabled
+        var search;
+		if(tUseNumbers){
+			search = query + " " + episode;
+		} else {
+			search = query;
+		}
+
+		var html = "<a target=\"_blank\" href=\"" + tSearchURL;  
+		html += escape(search);
+		html += "\">";
+
+		if (tIcon != "") {
+			html += "<img width=\"14\" heigth=\"14\" border=\"0\" src=\"" + tIcon + "\" alt=\"" + tShortname + "\">";
+		} else {
+			html += tShortname;
+		}
+			html += "</a>";
+		return html;
+	}
+}	
    
-  function addDownloadPrivateShowList(downloadURL) {
-    // iterate through series table
-    var seriesTable = document.getElementById("myepisodes_views").getElementsByClassName("mylist")[0];
-    
-    // Create Download column
-    var header = seriesTable.getElementsByClassName("header")[0];
-    var newCol = document.createElement('th');
-    newCol.setAttribute('title', "Download");
-    newCol.innerHTML = "D";
-    header.appendChild(newCol);
-    
-    // iterate through rows
-    var trs = seriesTable.getElementsByTagName("tr");
-    for (i = 0; i < trs.length; i++) {
-      var tr = trs[i];
-      if(tr.className == "header"){ continue; }
+function addDownloadColumn(tracker) {
+	// iterate through series table
+	var seriesTable = $("div#myepisodes_views table.mylist");
 
-      // Get series title
-      var showTitle = tr.getElementsByClassName("showname")[0].getElementsByTagName("a")[0].innerHTML;
-      
-      // Get episode number
-      var episode = "S" + tr.getElementsByClassName("longnumber")[0].innerHTML.replace("x", "E");
-      
-      // Add download link to each episode
-      var newCell = document.createElement("td");
-      newCell.innerHTML = downloadURL.getHTML(showTitle, episode);
-      tr.appendChild(newCell);
-    }
-  }
-  
-  // --------------- downloadURL --------------- 
-  // FORMAT: Tracker(shortName, iconURL, searchURL, useNumbers)
-  //     shortName - Alt display name for link
-  //     iconURL - icon displayed for link created
-  //     searchURL - URL that search string is appended to
-  //     useNumbers - if true adds the episode number to the search URL
-  //var downloadURL = new Tracker("EZTV", "http://eztv.ag/favicon.ico", "https://eztv.ag/search/", false);
-  downloadURL = new Tracker("Kickass", "https://kastatic.com/images/favicon.ico", "https://kickass.to/usearch/?field=seeders&sorder=desc&q=", true);
-  // --------------- END OF downloadURL --------------- 
+	// Create Download column
+	seriesTable.find("tr.header").append("<th title=\"Download\">D</th>");
 
-  //if($("title").text().indexOf("SeriesWatch") > -1){
-  //  addDownloadSeriesWatch(downloadURL);
-  //} else if($("title").text().indexOf("Private Show List") > -1){
-    addDownloadPrivateShowList(downloadURL);
-  //}  
-  
-})();
+	// iterate through rows
+	seriesTable.find("tr[class!=header]").each(function() {
+		// only add to the ones already out
+		if($(this).hasClass("past") || $(this).hasClass("today")){
+			// Get series title
+			var showTitle = $(this).find("td.showname a").text();
+
+			// Get episode number
+			var episode = "S" + $(this).find("td.longnumber").text().replace("x", "E");
+
+			// Add download link to each episode
+			$(this).append("<td align=\"center\">" + tracker.getHTML(showTitle, episode) + "</td>");		
+		}
+		else{
+			$(this).append("<td align=\"center\">-</td>");			
+		}
+	});
+}
+
+// add the extra column when the data is loaded 
+$('div#view').bind("DOMSubtreeModified",function(){
+	if($('div#view table.mylist')){
+		$('div#view').unbind("DOMSubtreeModified");
+		addDownloadColumn(tracker);
+	}
+});
+
